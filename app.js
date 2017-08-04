7fd1aa7344509fd083fbf8f7d5d1225dc30dd76c
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-08-04 10:21:29 -0400

    Add styling and comments section

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express       = require('express'),
     bodyParser    = require('body-parser'),
     mongoose      = require('mongoose'),
     Campground    = require('./models/campground'),
+    Comment       = require('./models/comment'),
     seedDb        = require('./seeds');
 
 
 mongoose.connect('mongodb://localhost/yelp_camp', {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 seedDb();
 
@@ -67,14 +68,35 @@ app.get('/campgrounds/:id', function(req, res) {
 
 // CREATE
 
-
 //NEW
 app.get('/campgrounds/:id/comments/new', function(req, res) {
-  res.render('comments/new');
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', {campground: campground});
+    }
+  });
 });
 
-
-
+app.post('/campgrounds/:id/comments', function(req, res) {
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      });
+    }
+  });
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening!");
