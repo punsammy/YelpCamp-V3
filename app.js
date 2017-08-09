7b983ca40ef7dbdef8d44fe0327758469c846ff0
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-08-09 10:40:35 -0400

    add login functionality

--- app.js
+++ app.js
@@ -136,6 +136,20 @@ app.post("/register", function(req, res){
   });
 });
 
+// login Routes
+app.get("/login", function(req,res){
+  res.render("login");
+});
+//handling login logic
+//uses passport-local-mongoose to authenticate user using whats in database(local), then redirects
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening!");
