c6d70d0703d746325c103cd8aa154edb54b70516
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-08-11 16:28:56 -0400

    add method-override package, show username of user for new campgrounds, styling changes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,11 @@ router.post('/', isLoggedIn, function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc};
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newCampground = {name: name, image: image, description: desc, author: author};
   Campground.create(newCampground, function(err, newlyCreated){
     if (err) {
       console.log(err);
