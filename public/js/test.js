2fcb821668db76f8c6ab9cf496102b9d3c909536
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-09-18 15:46:31 -0400

    add type text

---
+++ public/js/test.js
@@ -0,0 +1,8 @@
+$(function(){
+      $(".write").typed({
+          strings: ["Discover your next destination!", "Thanks for visiting"],
+          typeSpeed: 1,
+          loop: null,
+          backDelay: 500
+      });
+});
